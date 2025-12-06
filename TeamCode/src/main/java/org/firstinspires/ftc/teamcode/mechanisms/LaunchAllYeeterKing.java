cd8b08b1daa31a41316b0f9d8e1e9b0087bb6264
Bill Kidwell <46457910+billkidwell@users.noreply.github.com>
2025-12-05 20:12:15 -0500

    Added LaunchAllTeleOp
    Duplicated Yeeter King and added launchAll()
    Duplicated GamepadDrivingTeleOp and adjusted to use LaunchAllYeeterKing
    
    Bound Left Bumper to Launch All @ 800
    Bound Right Bumpter to Launch All @ 1100

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LaunchAllYeeterKing.java
@@ -0,0 +1,242 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class LaunchAllYeeterKing {
+
+    private boolean isYeeting;
+    private DcMotorEx yeetWheelLeft;
+    private DcMotorEx yeetWheelNotLeft;
+
+    private Servo servo1, servo2;
+
+    private double velocity = 0;
+
+    final double tolerance = 0.1;
+
+    private Telemetry telemetry;
+
+    private ElapsedTime timer;
+
+    private Eater eater = new Eater();
+
+    private enum LaunchState {
+        /**
+         * The default state
+         */
+        IDLE,
+
+        SPIN_UP,
+
+        READY,
+
+        LAUNCH,
+        /**
+         * In this state, we are actively waiting for the launch to occur.
+         * From here, we probably need to return to SPIN_UP to get back to launch
+         * velocity.
+         */
+        LAUNCHING,
+
+        LAUNCH3,
+
+        LAUNCHING3,
+    }
+
+    private boolean hasLaunched = false;
+    private boolean shotRequested = false;
+    private boolean isTripleShot = false;
+    public void launch() {
+        shotRequested = true;
+        isTripleShot = false;
+    }
+    public void launchAll() {
+        shotRequested = true;
+        isTripleShot = true;
+    }
+
+    public LaunchState getLaunchState() {
+        return launchState;
+    }
+
+    private LaunchState launchState = LaunchState.IDLE;
+
+    public void setDirection(DcMotor.Direction direction) {
+        if (direction == DcMotor.Direction.FORWARD) {
+            yeetWheelLeft.setDirection(DcMotor.Direction.REVERSE);
+            yeetWheelNotLeft.setDirection(DcMotor.Direction.FORWARD);
+        } else {
+            yeetWheelLeft.setDirection(DcMotor.Direction.FORWARD);
+            yeetWheelNotLeft.setDirection(DcMotor.Direction.REVERSE);
+        }
+    }
+
+    public void open()
+    {
+        servo1.setPosition(.15);
+        servo2.setPosition(.15);
+    }
+
+    public void push()
+    {
+        servo1.setPosition(.35);
+        servo2.setPosition(.35);
+    }
+    public void close()
+    {
+        servo1.setPosition(0);
+        servo2.setPosition(0);
+    }
+
+    public boolean toggleEater() {
+        return eater.toggle();
+    }
+
+    public void turnOnEater() {
+        eater.on();
+    }
+
+    public void turnOffEater() {
+        eater.off();
+    }
+
+    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+        timer = new ElapsedTime();
+        yeetWheelLeft = hardwareMap.get(DcMotorEx.class, "flywheel_left");
+
+        yeetWheelLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        yeetWheelLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        yeetWheelNotLeft = hardwareMap.get(DcMotorEx.class, "flywheel_right");
+
+        yeetWheelNotLeft.setDirection(DcMotor.Direction.FORWARD);
+
+        yeetWheelNotLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        this.telemetry = telemetry;
+
+        PIDFCoefficients coefficients = new PIDFCoefficients(300, 0, 0, 10);
+        yeetWheelLeft.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, coefficients);
+        yeetWheelNotLeft.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, coefficients);
+
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+        servo1.setDirection(Servo.Direction.REVERSE);
+
+        servo2 = hardwareMap.get(Servo.class, "servo2");
+        //servo2.setDirection(Servo.Direction.FORWARD);
+
+        eater.init(hardwareMap);
+    }
+
+    public void stop(){
+        yeetWheelLeft.setPower(0);
+        yeetWheelNotLeft.setPower(0);
+        launchState= LaunchState.IDLE;
+    }
+
+    public void printTelemetry(){
+        telemetry.addData("LaunchSate",getLaunchState());
+        telemetry.addData("leftWheel",yeetWheelLeft.getVelocity());
+        telemetry.addData("notLeftWheel",yeetWheelNotLeft.getVelocity());
+        telemetry.addData("shotRequested", shotRequested);
+        telemetry.addData("hadLaunched",hasLaunched);
+
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+    public void spinUp() {
+        this.launchState = LaunchState.SPIN_UP;
+        yeetWheelLeft.setVelocity(velocity);
+        yeetWheelNotLeft.setVelocity(velocity);
+    }
+
+    public boolean isReady() {
+        return launchState == LaunchState.READY;
+    }
+
+    public void update() {
+
+        // If we have already launched, and a new shot is requested,
+        // reset hasLaunched to track the next shot
+        if (hasLaunched == true && shotRequested == true) {
+            hasLaunched = false;
+        }
+
+        switch (launchState) {
+            case IDLE:
+                // telemetry.addData("LaunchState", launchState);
+                // telemetry.addData("LaunchVelocity",shootVelocity);
+                if (shotRequested) {
+                    launchState = LaunchState.SPIN_UP;
+                }
+                break;
+            case SPIN_UP:
+                close();
+                // telemetry.addData("LaunchState", launchState);
+                //telemetry.addData("LaunchVelocity",shootVelocity);
+                yeetWheelLeft.setVelocity(velocity);
+                yeetWheelNotLeft.setVelocity(velocity);
+
+                // Advance to LAUNCH when both motors are up to speed
+                if (yeetWheelLeft.getVelocity() >= (velocity - tolerance) &&
+                        yeetWheelNotLeft.getVelocity() >= (velocity - tolerance)) {
+                    launchState = LaunchState.READY;
+                }
+                break;
+
+            case READY:
+                if (hasLaunched == false && shotRequested == true) {
+                    launchState = launchState.LAUNCH;
+                }
+                break;
+
+            case LAUNCH:
+                eater.off(); // turn off the intake before pushing
+                push();
+                timer.reset();
+                launchState = LaunchState.LAUNCHING;
+                break;
+
+            case LAUNCH3:
+                eater.off();
+                open();
+                eater.on();
+                timer.reset();
+                launchState = LaunchState.LAUNCHING3;
+                break;
+
+            case LAUNCHING:
+                hasLaunched = true;
+                shotRequested = false;
+                if (timer.seconds() > .8) {
+                    close();
+                }
+                if (timer.seconds() > 1.8) {
+                    launchState = LaunchState.SPIN_UP;
+                    eater.on(); // Re-engage the intake after closing
+                }
+                break;
+
+            case LAUNCHING3:
+                hasLaunched = true;
+                shotRequested = false;
+                if (timer.seconds() > 2) {
+                    close();
+                    launchState = LaunchState.SPIN_UP;
+                }
+                break;
+        }
+
+    }
+}
